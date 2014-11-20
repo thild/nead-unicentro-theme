bac8922e97f2f234e2e2414d86cf5cfad4062e62
Tony Alexander Hild <tony_hild@yahoo.com>
2014-11-20 17:19:50 -0200

    initial translation

---
+++ lang/pt_br/theme_unicentro.php
@@ -0,0 +1,79 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for unicentro details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme Unicentro lang file.
+ *
+ * @package    theme_unicentro
+ * @copyright  2014 Frédéric Massart, Tony Alexander Hild
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['backgroundfixed'] = 'Ancorar fundo';
+$string['backgroundfixed_desc'] = 'Use esta configuração para ancorar a imagem de fundo na página.';
+$string['backgroundimage'] = 'Imagem de fundo';
+$string['backgroundimage_desc'] = 'A imagem que será mostra como fundo deste sítio.';
+$string['backgroundposition'] = 'Posição do fundo';
+$string['backgroundposition_desc'] = 'A posição da imagem de fundo.';
+$string['backgroundpositioncenterbottom'] = 'Centralizado abaixo';
+$string['backgroundpositioncentercenter'] = 'Centralizado ao meio';
+$string['backgroundpositioncentertop'] = 'Centralizado acima';
+$string['backgroundpositionleftbottom'] = 'Esquerda abaixo';
+$string['backgroundpositionleftcenter'] = 'Esquerda centralizado';
+$string['backgroundpositionlefttop'] = 'Esquerda acima';
+$string['backgroundpositionrightbottom'] = 'Direita abaixo';
+$string['backgroundpositionrightcenter'] = 'Direita centralizado';
+$string['backgroundpositionrighttop'] = 'Direita acima';
+$string['backgroundrepeat'] = 'Repetir fundo';
+$string['backgroundrepeat_desc'] = 'Define a forma que a imagem de fundo será repetida.';
+$string['backgroundrepeatnorepeat'] = 'Sem repetição';
+$string['backgroundrepeatrepeat'] = 'Repetir';
+$string['backgroundrepeatrepeatx'] = 'Repetir horizontalmente';
+$string['backgroundrepeatrepeaty'] = 'Repetir verticalmente';
+$string['bodybackground'] = 'Cor de fundo';
+$string['bodybackground_desc'] = 'A cor principal usada como fundo.';
+$string['choosereadme'] = '<p>Unicentro é um tema que permite que você facilmente personalize as configurações visuais do Moodle a partir da interface web.</p>
+<p>Visite as configurações de administração para alterar cores, adicionar uma imagem como fundo, adicionar seu logotipo e mais.</p>';
+
+$string['configtitle'] = 'Unicentro';
+$string['contentbackground'] = 'Cor de fundo do conteúdo principal';
+$string['contentbackground_desc'] = 'Cor de fundo do conteúdo principal do sítio. Deixe vazio para nenhuma.';
+
+$string['customcss'] = 'CSS Personalizado';
+$string['customcssdesc'] = 'Whatever CSS rules you add to this textarea will be reflected in every page, making for easier customization of this theme.';
+
+$string['footnote'] = 'Footnote';
+$string['footnotedesc'] = 'Whatever you add to this textarea will be displayed in the footer throughout your Moodle site.';
+
+$string['invert'] = 'Invert navbar';
+$string['invertdesc'] = 'Swaps text and background for the navbar at the top of the page between black and white.';
+
+$string['linkcolor'] = 'Link colour';
+$string['linkcolor_desc'] = 'The colour of the links.';
+$string['logo'] = 'Logo';
+$string['logodesc'] = 'Please upload your custom logo here if you want to add it to the header.<br>
+If the height of your logo is unicentro than 75px add the following CSS rule to the Custom CSS box below.<br>
+a.logo {height: 100px;} or whatever height in pixels the logo is.';
+
+$string['pluginname'] = 'Unicentro';
+
+$string['region-side-post'] = 'Right';
+$string['region-side-pre'] = 'Left';
+$string['secondarybackground'] = 'Secondary background colour';
+$string['secondarybackground_desc'] = 'The background colour of any secondary content, such as blocks.';
+$string['textcolor'] = 'Text colour';
+$string['textcolor_desc'] = 'The colour of the text.';
+
