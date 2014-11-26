2df087a81cd51aac58681c45bec75e4c45df8fbf
Tony Alexander Hild <tony_hild@yahoo.com>
2014-11-26 13:54:38 -0200

    Some styling

--- lib.php
+++ lib.php
@@ -173,3 +173,51 @@ function theme_unicentro_set_customcss($css, $customcss) {
 
     return $css;
 }
+
+/**
+ * Returns an object containing HTML for the areas affected by settings.
+ *
+ * Do not add Unicentro specific logic in here, child themes should be able to
+ * rely on that function just by declaring settings with similar names.
+ *
+ * @param renderer_base $output Pass in $OUTPUT.
+ * @param moodle_page $page Pass in $PAGE.
+ * @return stdClass An object with the following properties:
+ *      - navbarclass A CSS class to use on the navbar. By default ''.
+ *      - heading HTML to use for the heading. A logo if one is selected or the default heading.
+ *      - footnote HTML to use as a footnote. By default ''.
+ */
+function theme_unicentro_get_html_for_settings(renderer_base $output, moodle_page $page) {
+    global $CFG;
+    $return = new stdClass;
+
+    $return->navbarclass = '';
+    if (!empty($page->theme->settings->invert)) {
+        $return->navbarclass .= ' navbar-inverse';
+    }
+
+    if (!empty($page->theme->settings->logo)) {
+        $courseid = $page->course->id;
+        $context = context_course::instance($courseid);
+        //print_object($page->course);
+        $theme = theme_config::load('unicentro');
+        $logo = $theme->setting_file_url('logo', 'logo');
+        $img = html_writer::empty_tag('img', array('src' => $logo, 'class' => 'logo-placeholder hidden'));
+        $text = $page->course->summary;
+        $return->heading = html_writer::link($CFG->wwwroot, $img.$text, array('title' => get_string('home'), 'class' => 'logo'));
+    } else {
+        $return->heading = $output->page_heading();
+    }
+
+    $return->footnote = '';
+    if (!empty($page->theme->settings->footnote)) {
+        $return->footnote = '<div class="footnote text-center">'.format_text($page->theme->settings->footnote).'</div>';
+    }
+
+    return $return;
+}
+
+function theme_unicentro_page_init(moodle_page $page) {
+    $page->requires->jquery();
+    $page->requires->jquery_plugin('bootstrap', 'theme_unicentro');
+}
